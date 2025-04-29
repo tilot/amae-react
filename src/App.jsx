8a6bcc970e61bb713c7b8d04ef6adf8c21a7aaf3
Tilot <tilit51@gmail.com>
2025-04-29 11:43:13 +0200

    test loginbutton et ajout de cgu

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { LoginButton } from './components/LoginButton/LoginButton.jsx'
+import LoginButton from './components/LoginButton/LoginButton.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
