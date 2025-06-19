20733ecfdb995e7512facf5f92cbced7b0d0cc79
Tilot <tilit51@gmail.com>
2025-06-19 23:51:21 +0200

    modification de la page d'erreur

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,18 +1,23 @@
 /* fichier 404.jsx */
 import React from "react";
 import { useRouteError } from "react-router-dom";
+import './404.css';
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="error-page-404">
+      <div className="error-box-404">
+        <h1 className="error-title-404">Oups !</h1>
+        <p className="error-message-404">Désolé, une erreur inattendue s'est produite.</p>
+        {error && (
+          <p className="error-detail-404">
+            <i>{error.statusText || error.message}</i>
+          </p>
+        )}
+      </div>
     </div>
   );
 }
